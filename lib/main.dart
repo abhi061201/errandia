929898e73a6578b133cfa57bf043d2e476c764e6
abhi061201 <abhishekbmdu1@gmail.com>
2023-08-02 12:06:23 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,22 @@
-import 'package:errandia/app/modules/buiseness/view/add_business_view.dart';
-import 'package:errandia/app/modules/buiseness/view/businesses_view.dart';
-import 'package:errandia/app/modules/buiseness/view/errandia_business_view.dart';
-import 'package:errandia/app/modules/buiseness/view/manage_business_view.dart';
-import 'package:errandia/app/modules/categories/view/categories.dart';
-import 'package:errandia/app/modules/home/controller/home_controller.dart';
-import 'package:errandia/app/modules/home/view/home_view.dart';
-import 'package:errandia/app/modules/home/view/home_view_1.dart';
-import 'package:errandia/app/modules/products/view/filter_product_view.dart';
-import 'package:errandia/app/modules/products/view/products_send_enquiry.dart';
-import 'package:errandia/app/modules/reviews/views/add_review.dart';
+import 'package:errandia/app/modules/Billing/view/Billing_history_view.dart';
+import 'package:errandia/app/modules/Enquiries/view/enquiries_view.dart';
+import 'package:errandia/app/modules/errands/view/errand_detail_view.dart';
+import 'package:errandia/app/modules/errands/view/errand_view.dart';
+import 'package:errandia/app/modules/following/view/following_view.dart';
+import 'package:errandia/app/modules/manage_review/view/manage_review_view.dart';
 import 'package:errandia/app/modules/splashScreen/splash_screen.dart';
-import 'package:errandia/common/random_ui/ui_23.dart';
-import 'package:errandia/common/random_ui/run_an_errand.dart';
-import 'package:errandia/app/modules/products/view/product_view.dart';
-import 'package:errandia/app/modules/reviews/views/review_view.dart';
+import 'package:errandia/app/modules/subscribers/view/subscriber_view.dart';
+import 'package:errandia/app/modules/subscription/view/manage_subscription_view.dart';
+import 'package:errandia/app/modules/subscription/view/renew_subscription.dart';
 import 'package:errandia/languages/language.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-import 'app/modules/products/view/manage_products_view.dart';
-
-void main()async {
+void main() async {
   await GetStorage.init();
- 
+
   runApp(const ErrandiaApp());
 }
 
@@ -34,27 +26,22 @@ class ErrandiaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     print('Author=> Abhishek Gupta');
     return GetMaterialApp(
-      
       translations: Languages(),
       locale: Locale('en', 'US'),
-      fallbackLocale: Locale('fr','CA'),
+      fallbackLocale: Locale('fr', 'CA'),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
-        textTheme: TextTheme(
-          
-        ),
+        textTheme: TextTheme(),
         primaryColor: Color(0xff113d6b),
-        primaryTextTheme: TextTheme(
-          
-        ),
+        primaryTextTheme: TextTheme(),
       ),
-      home: manage_product_view(),
+      home: splash_screen(),
     );
   }
 }
